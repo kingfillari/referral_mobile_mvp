fc84c167aeabfd435caf42c5a85b1f1afc04ddd2
KingFillari <gebrefillimon@gmail.com>
2026-03-11 00:24:34 +0300

    update services/theme_service.dart

---
+++ lib/services/theme_service.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// ThemeService: Manage dark/light theme
+class ThemeService with ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  ThemeMode get themeMode => _themeMode;
+
+  bool get isDarkMode => _themeMode == ThemeMode.dark;
+
+  void toggleTheme() {
+    if (_themeMode == ThemeMode.dark) {
+      _themeMode = ThemeMode.light;
+    } else {
+      _themeMode = ThemeMode.dark;
+    }
+    notifyListeners();
+  }
+}
